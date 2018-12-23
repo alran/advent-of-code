982b392210bb45353e34b1d80509f28f8ab39221
Alyssa R <alran@users.noreply.github.com>
2018-12-22 18:22:19 -0800

    Create Program.cs

---
+++ 2018/C/Program.cs
@@ -0,0 +1,57 @@
+using System;
+using NUnit.Framework;
+
+namespace advent_of_code_2018
+{
+    [TestFixture]
+    public class UnitTests
+    {
+        [Test]
+        public void TestDay1Part1()
+        {
+            string[] input = System.IO.File.ReadAllLines("../../../../inputs/day-1.txt");
+            int frequency = new Day1().GetFrequency(input);
+            Assert.AreEqual(402, frequency);
+        }
+
+        [Test]
+        public void TestDay1Part2()
+        {
+            string[] input = System.IO.File.ReadAllLines("../../../../inputs/day-1.txt");
+            int frequency = new Day1().ReachesSameFrequency(input);
+            Assert.AreEqual(481, frequency);
+        }
+
+        [Test]
+        public void TestDay2Part1()
+        {
+            string[] input = System.IO.File.ReadAllLines("../../../../inputs/day-2.txt");
+            int answer = new Day2().PartOne(input);
+            Assert.AreEqual(5976, answer);
+        }
+
+        [Test]
+        public void TestDay2Part2()
+        {
+            string[] input = System.IO.File.ReadAllLines("../../../../inputs/day-2.txt");
+            string answer = new Day2().PartTwo(input);
+            Assert.AreEqual("xretqmmonskvzupalfiwhcfdb", answer);
+        }
+
+        [Test]
+        public void TestDay3Part1()
+        {
+            string[] input = System.IO.File.ReadAllLines("../../../../inputs/day-3.txt");
+            int answer = new Day3().PartOne(input);
+            Assert.AreEqual(111485, answer);
+        }
+
+        [Test]
+        public void TestDay3Part2()
+        {
+            string[] input = System.IO.File.ReadAllLines("../../../../inputs/day-3.txt");
+            int answer = new Day3().PartTwo(input);
+            Assert.AreEqual(113, answer);
+        }
+    }
+}
